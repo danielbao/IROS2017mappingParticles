902f3946fbf3a8526a7ad44d605110e2c5246654
danielbao <danielbao98@gmail.com>
2018-02-12 00:25:08 -0600

    More Data; better reorganization
    Janus Particles 4 v itself at 1 isn't a good comparison. It outdoes
    Closest probably because it is a BFS. Will need more looking at.

--- code/matlab/1_16_data/ANOVA_code.m
+++ code/matlab/1_16_data/ANOVA_code.m
@@ -4,17 +4,16 @@
 % Cycle through the data
 % Store the last value of nodecount
 % Do an anova, get p-value, be done!
-cell_data=zeros(size(janustemprun,2),2);
+cell_data=zeros(size(janus1temprun,2),2);
 for i=100:50:100%size(temprun,1) %set the range of values for the function. For now it will run for no of robots=500,2000
-    for j=1:size(janustemprun,2)%set the numner of iterations for the function
-        cell_data(j,1)=janustemprun(i,j).nodecount(250);
-    end
-end
-for i=100:50:100%size(temprun,1) %set the range of values for the function. For now it will run for no of robots=500,2000
-    for j=1:size(temprun,2)%set the numner of iterations for the function
-        cell_data(j,2)=temprun(i,j).nodecount(250);
+    for j=1:size(janus1temprun,2)%set the numner of iterations for the function
+        cell_data(j,1)=janus2temprun(i,j).nodecount(250);
+        cell_data(j,2)=janus1temprun(i,j).nodecount(250);
+
     end
 end
 [p,tbl,stats] = anova1(cell_data);
-ylabel(['Total amount of cells explored']);
-xlabel(['Col 1 is n Janus Particles; Col 2 is the same particle']);
+[ht,pt] = ttest(cell_data(:,1), cell_data(:,2));
+title('LeafMap exploration for Janusparticles at 250 moves');
+ylabel('Total amount of cells explored');
+xlabel('Col 1 is 2 Janus Particles; Col 2 is the same particle');
