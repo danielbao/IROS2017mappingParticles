923096d01e738f38ce6c90bb34c2683972fc593e
danielbao <danielbao98@gmail.com>
2018-02-23 17:18:34 -0600

    More 2D Data

--- code/matlab/Data/New Data/1D Data/movecount_plot.m
+++ code/matlab/Data/New Data/1D Data/movecount_plot.m
@@ -1,6 +1,8 @@
 run1=struct;
 run2=struct;
-for i=100:50:900 %set the range of values for the function. For now it will run for no of robots=500,2000
+run3=struct;
+run4=struct;
+for i=100:20:400 %set the range of values for the function. For now it will run for no of robots=500,2000
 %     for j=1:50 %set the numner of iterations for the function
 %         [janus2temprun(i,j).movecount,janus2temprun(i,j).k,janus2temprun(i,j).nodecount,janus2temprun(i,j).init_config] = JanusSpecies(i,j,250,0,0.5,0,0.5,0,0); 
 %         clf
@@ -16,19 +18,28 @@ for i=100:50:900 %set the range of values for the function. For now it will run 
     run2(i).movecount=mean([janus2temprun(i,:).movecount]);
     run2(i).stderr=std([janus2temprun(i,:).movecount]);
     run2(i).k=mean([janus2temprun(i,:).k]);
+    run3(i).movecount=mean([janus3temprun(i,:).movecount]);
+    run3(i).stderr=std([janus3temprun(i,:).movecount]);
+    run3(i).k=mean([janus3temprun(i,:).k]);
+    run4(i).movecount=mean([janus4temprun(i,:).movecount]);
+    run4(i).stderr=std([janus4temprun(i,:).movecount]);
+    run4(i).k=mean([janus4temprun(i,:).k]);    
 end
 figure;
 hold on;
 shadedErrorBar([run1.k],[run1.movecount],[run1.stderr],'g');
 shadedErrorBar([run2.k],[run2.movecount],[run2.stderr],'b');
+shadedErrorBar([run4.k],[run4.movecount],[run4.stderr],'c');
 alpha 0.5;
-title('1D Simulation for Janus Particles on 100 space Linear Map');
+title('2D Simulation for Janus Particles on 500 space Rectangular Map');
 xlabel('Number of Particles (n)');
 ylabel('Number of Moves (k)');
 h=findobj(gca,'Type','Line');
-legend([h(1) h(4)],'1 Janus Species', '2 Janus Species');
+legend([h(7) h(4) h(1)], '1 Type Janus', '2 Type Janus','4 Type Janus')
+
+
 %Instructions of getting a good legend for the right colors
 % h=findobj(gca,'Type','Line')
 % Depending on how you plotted it, the first 3 should be the first
 % plot, the 2nd 3 should be the 2nd plot
-% Call legend(h(lineindex),'NameOfLine'); for each line!
+% Call legend([h(lineindex)],'NameOfLine'); for each line!
